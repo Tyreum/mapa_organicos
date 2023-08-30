19dbd7c23120dc9052f14df8a58d333a7c38f9f1
André de Queiroz <andrepatrinicola@gmail.com>
2023-08-30 20:25:20 -0300

    configuracoes estaticos e templates

---
+++ accounts/static/js/src/index.js
@@ -0,0 +1,64 @@
+
+
+function teste(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+
+
+function teste1(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+
+
+
+function teste2(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+
+
+
+function teste3(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+
+
+
+function teste4(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+
+
+
+function teste5(){
+    for(i = 0; i < 10; i++){
+        const oi = "";
+        console.log("ola")
+    }
+}
+
+teste()
+teste1()
+teste2()
+teste3()
+teste4()
+teste5()
--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   entry: {
     accounts : "./accounts/static/js/src/index.js",
-    home : "./accounts/static/js/src/index.js"
+    home : "./home/static/js/src/index.js"
   },
   output: {
-    path: path.resolve(__dirname, 'apps', 'static', 'assets', 'js'),
-    filename: '[name]/static/js/build/bundle.js',
+    path: path.resolve(__dirname),
+    filename: '[name]/static/js/build/[name].bundle.js',
   },
 };
