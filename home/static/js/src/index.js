c3881205047645425f5d26bd7b0038429ce55cb2
André de Queiroz Patrinicola <andrepatrinicola@gmail.com>
2023-11-28 18:05:11 -0300

    adiciona mapa

--- home/static/js/src/index.js
+++ home/static/js/src/index.js
@@ -1,12 +1,13 @@
-
-
+import L from 'leaflet';
+// import 'leaflet/dist/leaflet.css'
 
 
 function teste(){
-    for(i = 0; i < 10; i++){
-        const oi = "";
-        console.log("ola")
-    }
+    const ma = L.map('map').setView([-23.555, -46.635], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(ma);  
 }
 
 
