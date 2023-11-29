ef2a30f56eb62dadc13a95befef19ab2534892ee
André de Queiroz <andrepatrinicola@gmail.com>
2023-11-29 17:34:17 -0300

    termina filtros do mapa

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class UserConsumidor(models.Model):
+class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    logradouro = models.CharField(max_length=30, null=True, blank=True)
+    numero = models.IntegerField(null=True, blank=True)
+    latitude = models.CharField(max_length=20, null=True, blank=True)
+    longitude = models.CharField(max_length=20, null=True, blank=True)
